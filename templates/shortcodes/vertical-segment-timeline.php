bf2704f6752f715c65b827523bd2f68a7968e4de
OlegApanovich <dollar4444@gmail.com>
2025-02-15 17:24:48 +0200

    Css class renaming

--- templates/shortcodes/vertical-segment-timeline.php
+++ templates/shortcodes/vertical-segment-timeline.php
@@ -9,18 +9,18 @@
 
 defined( 'ABSPATH' ) || exit;
 ?>
-<div <?php $_this->output_shortcode_wrapper_attributes( [ 'class' => 'chargewp-horizontal-section-timeline-content' ] ); ?>>
-	<ul class="chargewp-horizontal-section-timeline-list">
+<div <?php $_this->output_shortcode_wrapper_attributes( [ 'class' => 'chargewp-vertical-segment-timeline-content' ] ); ?>>
+	<ul class="chargewp-vertical-segment-timeline-list">
 		<?php echo do_shortcode( $content ); ?>
 	</ul>
 </div>
 
 <style>
-	<?php $_this->output_style_shortcode_id(); ?> .chargewp-horizontal-section-timeline-list {
+	<?php $_this->output_style_shortcode_id(); ?> .chargewp-vertical-segment-timeline-list {
 		border-left: 4px solid <?php echo esc_attr( $atts['baseline_background'] ); ?>;
 		background: <?php echo esc_attr( $atts['content_background_color'] ); ?>;
 	}
-	.chargewp-horizontal-section-timeline-content<?php $_this->output_style_shortcode_id(); ?> {
+	.chargewp-vertical-segment-timeline-content<?php $_this->output_style_shortcode_id(); ?> {
 		background: <?php echo esc_attr( $atts['background_color'] ); ?>;
 	}
 </style>
