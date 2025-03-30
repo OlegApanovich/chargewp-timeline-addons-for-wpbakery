a329fad04038e6cabf053610bc499ae2002b8094
OlegApanovich <dollar4444@gmail.com>
2025-03-30 12:08:31 +0300

    Improve tilewind css resets applying

--- assets/js/shortcodes-common/tailwind-cdn-after-setup.js
+++ assets/js/shortcodes-common/tailwind-cdn-after-setup.js
@@ -1,7 +1,11 @@
-(function () {
-    tailwind.config = {
-        corePlugins: {
-            preflight: false // Disables Tailwind's CSS reset
-        }
-    };
-}())
+(function checkTailwindLoaded() {
+    if (window.tailwind && window.tailwind.config) {
+        tailwind.config = {
+            corePlugins: {
+                preflight: false // Disables Tailwind's CSS reset
+            }
+        };
+    } else {
+        setTimeout(checkTailwindLoaded, 50); // Try again in 50ms
+    }
+})();
