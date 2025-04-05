a68d2d2fadcf847230c02bc47301fba5988670d9
OlegApanovich <dollar4444@gmail.com>
2025-04-05 17:27:18 +0300

    Adjust params for a marker of tailwind-timeline element

--- config/shortcodes/tailwind-vertical-classic-timeline-item.php
+++ config/shortcodes/tailwind-vertical-classic-timeline-item.php
@@ -6,63 +6,155 @@
  */
 
 use ChargewpWpbTimeline\Shortcodes\ChargeWpbShortcodeEmpty;
+use ChargewpWpbTimeline\CustomHeadingIntegration;
+use ChargewpWpbTimeline\CustomIconIntegration;
 
 defined( 'ABSPATH' ) || exit;
 
-$params = [
+$custom_heading_integration = new CustomHeadingIntegration();
+$custom_icon_integration    = new CustomIconIntegration();
+
+$params = array_merge(
     [
-        'type'        => 'chargewp-notice',
-        'level'       => 'warning',
-        'notice'      => '<b>The Tailwind environment may apply additional CSS styles to the entire current page, not just the current element. Please check the page output after using Tailwind elements.</b>',
-        'param_name'  => 'tailwind_notice',
-    ],
-    [
-        'type'        => 'dropdown',
-        'value'       => [
-            esc_html__( 'Left', 'chargewp-timeline-addons-for-wpbakery' ) => 'left',
-            esc_html__( 'Right', 'chargewp-timeline-addons-for-wpbakery' ) => 'right',
+        [
+            'type'        => 'chargewp-notice',
+            'level'       => 'warning',
+            'notice'      => '<b>The Tailwind environment may apply additional CSS styles to the entire current page, not just the current element. Please check the page output after using Tailwind elements.</b>',
+            'param_name'  => 'tailwind_notice',
         ],
-        'heading'     => esc_html__( 'Rotation', 'chargewp-timeline-addons-for-wpbakery' ),
-        'param_name'  => 'rotation',
-        'description' => esc_html__( 'Choose right or left item rotation for timeline item.', 'chargewp-timeline-addons-for-wpbakery' ),
-    ],
-    [
-        'type'        => 'dropdown',
-        'value'       => [
-            esc_html__( 'Circle', 'chargewp-timeline-addons-for-wpbakery' ) => 'circle',
-            esc_html__( 'Square', 'chargewp-timeline-addons-for-wpbakery' ) => 'square',
-            esc_html__( 'No', 'chargewp-timeline-addons-for-wpbakery' ) => 'no',
+        [
+            'type'        => 'dropdown',
+            'value'       => [
+                esc_html__( 'Left', 'chargewp-timeline-addons-for-wpbakery' ) => 'left',
+                esc_html__( 'Right', 'chargewp-timeline-addons-for-wpbakery' ) => 'right',
+            ],
+            'heading'     => esc_html__( 'Rotation', 'chargewp-timeline-addons-for-wpbakery' ),
+            'param_name'  => 'rotation',
+            'description' => esc_html__( 'Choose right or left item rotation for timeline item.', 'chargewp-timeline-addons-for-wpbakery' ),
         ],
-        'heading'     => esc_html__( 'Marker Type', 'chargewp-timeline-addons-for-wpbakery' ),
-        'param_name'  => 'marker_type',
-        'description' => esc_html__( 'Choose marker type.', 'chargewp-timeline-addons-for-wpbakery' ),
-        'group'       => 'Marker',
+        [
+            'type'        => 'dropdown',
+            'value'       => [
+                esc_html__( 'Circle', 'chargewp-timeline-addons-for-wpbakery' ) => 'circle',
+                esc_html__( 'Square', 'chargewp-timeline-addons-for-wpbakery' ) => 'square',
+                esc_html__( 'No', 'chargewp-timeline-addons-for-wpbakery' ) => 'no',
+            ],
+            'heading'     => esc_html__( 'Marker Type', 'chargewp-timeline-addons-for-wpbakery' ),
+            'param_name'  => 'marker_type',
+            'description' => esc_html__( 'Choose marker type.', 'chargewp-timeline-addons-for-wpbakery' ),
+            'group'       => 'Marker',
+        ],
+        [
+            'type'        => 'colorpicker',
+            'value'       => '#ffffff',
+            'heading'     => esc_html__( 'Marker background color', 'chargewp-timeline-addons-for-wpbakery' ),
+            'param_name'  => 'marker_background_color',
+            'description' => esc_html__( 'Select custom color for marker background.', 'chargewp-timeline-addons-for-wpbakery' ),
+            'dependency'  => [ 'element' => 'marker_type', 'value' => [ 'circle', 'square' ] ],
+            'group'       => 'Marker',
+        ],
+        [
+            'type'        => 'colorpicker',
+            'value'       => '#EEEE22',
+            'heading'     => esc_html__( 'Marker background hover color', 'chargewp-timeline-addons-for-wpbakery' ),
+            'param_name'  => 'marker_background_hover_color',
+            'description' => esc_html__( 'Select custom color for marker background hover.', 'chargewp-timeline-addons-for-wpbakery' ),
+            'dependency'  => [ 'element' => 'marker_type', 'value' => [ 'circle', 'square' ] ],
+            'group'       => 'Marker',
+        ],
+        [
+            'type'        => 'dropdown',
+            'value'       => [
+                esc_html__( 'Slim', 'chargewp-timeline-addons-for-wpbakery' ) => '2',
+                esc_html__( 'Middle', 'chargewp-timeline-addons-for-wpbakery' ) => '4',
+                esc_html__( 'Bold', 'chargewp-timeline-addons-for-wpbakery' ) => '8',
+            ],
+            'heading'     => esc_html__( 'Marker Border Type', 'chargewp-timeline-addons-for-wpbakery' ),
+            'param_name'  => 'marker_border_type',
+            'description' => esc_html__( 'Choose marker border type.', 'chargewp-timeline-addons-for-wpbakery' ),
+            'dependency'  => [ 'element' => 'marker_type', 'value' => [ 'circle', 'square' ] ],
+            'group'       => 'Marker',
+        ],
+        [
+            'type'        => 'colorpicker',
+            'value'       => '#cccccc',
+            'heading'     => esc_html__( 'Marker border color', 'chargewp-timeline-addons-for-wpbakery' ),
+            'param_name'  => 'marker_border_color',
+            'description' => esc_html__( 'Select custom color for marker border.', 'chargewp-timeline-addons-for-wpbakery' ),
+            'dependency'  => [ 'element' => 'marker_type', 'value' => [ 'circle', 'square' ] ],
+            'group'       => 'Marker',
+        ],
+        [
+            'type'        => 'colorpicker',
+            'value'       => '#EEEE2278',
+            'heading'     => esc_html__( 'Marker border hover color', 'chargewp-timeline-addons-for-wpbakery' ),
+            'param_name'  => 'marker_border_hover_color',
+            'description' => esc_html__( 'Select custom color for marker border hover.', 'chargewp-timeline-addons-for-wpbakery' ),
+            'dependency'  => [ 'element' => 'marker_type', 'value' => [ 'circle', 'square' ] ],
+            'group'       => 'Marker',
+        ],
+        [
+            'type'        => 'chargewp-number',
+            'value'       => '44',
+            'min'         => '15',
+            'max'         => '100',
+            'step'        => '1',
+            'heading'     => esc_html__( 'Marker Width', 'chargewp-timeline-addons-for-wpbakery' ),
+            'param_name'  => 'marker_width',
+            'description' => esc_html__( 'Set custom width in px from 15 to 100 for marker.', 'chargewp-timeline-addons-for-wpbakery' ),
+            'group'       => 'Marker',
+            'dependency'  => [ 'element' => 'marker_type', 'value' => [ 'circle', 'square' ] ],
+            'title'       => esc_html__( 'px', 'chargewp-timeline-addons-for-wpbakery' ),
+        ],
+        [
+            'type'        => 'chargewp-number',
+            'value'       => '44',
+            'min'         => '15',
+            'max'         => '100',
+            'step'        => '1',
+            'heading'     => esc_html__( 'Marker Height', 'chargewp-timeline-addons-for-wpbakery' ),
+            'param_name'  => 'marker_height',
+            'description' => esc_html__( 'Set custom height in px from 15 to 100 for marker.', 'chargewp-timeline-addons-for-wpbakery' ),
+            'group'       => 'Marker',
+            'dependency'  => [ 'element' => 'marker_type', 'value' => [ 'circle', 'square' ] ],
+            'title'       => esc_html__( 'px', 'chargewp-timeline-addons-for-wpbakery' ),
+        ],
+        [
+            'type'        => 'dropdown',
+            'value'       => [
+                esc_html__( 'Solid', 'chargewp-timeline-addons-for-wpbakery' ) => 'solid',
+                esc_html__( 'Dotted', 'chargewp-timeline-addons-for-wpbakery' ) => 'dotted',
+                esc_html__( 'Dashed', 'chargewp-timeline-addons-for-wpbakery' ) => 'dashed',
+            ],
+            'heading'     => esc_html__( 'Marker Border Style', 'chargewp-timeline-addons-for-wpbakery' ),
+            'param_name'  => 'marker_border_style',
+            'description' => esc_html__( 'Choose marker border style.', 'chargewp-timeline-addons-for-wpbakery' ),
+            'dependency'  => [ 'element' => 'marker_type', 'value' => [ 'circle', 'square' ] ],
+            'group'       => 'Marker',
+        ],
+        [
+            'type'        => 'dropdown',
+            'value'       => [
+                0 => '0',
+                15=> '15',
+                30 => '30',
+                45 => '45',
+                90 => '90',
+                180 => '180',
+                270 => '270',
+            ],
+            'heading'     => esc_html__( 'Marker Rotation Degree', 'chargewp-timeline-addons-for-wpbakery' ),
+            'param_name'  => 'marker_rotation_degree',
+            'description' => esc_html__( 'Choose rotation degree.', 'chargewp-timeline-addons-for-wpbakery' ),
+            'dependency'  => [ 'element' => 'marker_type', 'value' => [ 'circle', 'square' ] ],
+            'group'       => 'Marker',
+        ],
     ],
-    [
-        'type'        => 'colorpicker',
-        'value'       => '#ffffff',
-        'heading'     => esc_html__( 'Marker background color', 'chargewp-timeline-addons-for-wpbakery' ),
-        'param_name'  => 'marker_background_color',
-        'description' => esc_html__( 'Select custom color for marker background.', 'chargewp-timeline-addons-for-wpbakery' ),
-        'dependency'  => [ 'element' => 'marker_type', 'value' => [ 'circle', 'square' ] ],
-        'group'       => 'Marker',
-    ],
-    [
-        'type'        => 'chargewp-number',
-        'value'       => '1',
-        'min'         => '0',
-        'max'         => '10',
-        'step'        => '1',
-        'heading'     => esc_html__( 'Marker Border', 'chargewp-timeline-addons-for-wpbakery' ),
-        'param_name'  => 'marker_border_width',
-        'description' => esc_html__( 'Set custom width from 0 to 10 for marker border.', 'chargewp-timeline-addons-for-wpbakery' ),
-        'group'       => 'Marker',
-        'dependency'  => [ 'element' => 'marker_type', 'value' => [ 'circle', 'square' ] ],
-    ],
-];
+    $custom_icon_integration->add_icon_checkbox( [ 'background_style', 'align', 'link', 'background_color', 'custom_background_color', 'size' ] )
+);
 
 return [
-	'name'                    => __( 'Tailwind Vertical Classic Timeline', 'chargewp-timeline-addons-for-wpbakery' ),
+	'name'                    => __( 'Tailwind Vertical Classic Timeline Item', 'chargewp-timeline-addons-for-wpbakery' ),
 	'base'                    => 'chargewp-tailwind-vertical-classic-timeline-item',
 	'class'                   => 'chargewp-tailwind-vertical-classic-timeline-item',
 	'description'             => __( 'Timeline item for "Tailwind Vertical Classic Timeline" element.', 'chargewp-timeline-addons-for-wpbakery' ),
--- templates/shortcodes/tailwind-vertical-classic-timeline-item.php
+++ templates/shortcodes/tailwind-vertical-classic-timeline-item.php
@@ -9,6 +9,8 @@
 
 defined( 'ABSPATH' ) || exit;
 
+use ChargewpWpbTimeline\CustomIconIntegration;
+$custom_icon_integration = new CustomIconIntegration();
 ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <div <?php $_this->output_shortcode_wrapper_attributes( [ 'class' => 'chargewp-tailwind-vertical-classic-timeline-item-container relative flex flex-col sm:flex-row items-center sm:items-center justify-between group' ] ); ?>>
@@ -32,66 +34,57 @@ defined( 'ABSPATH' ) || exit;
                 <h3 class="text-lg sm:text-xl md:text-2xl font-playfair text-gray-900 mt-2">Digital Renaissance</h3>
                 <p class="mt-2 text-sm sm:text-base text-gray-600 font-lato">The emergence of revolutionary digital platforms transforms modern journalism and storytelling methods.</p>
             </div>
-                <?php
-                    switch ($atts['marker_type']) {
-                        case 'circle':
-                            ?>
-                            <div class="
-                                absolute
-                                left-1/2
-                                transform
-                                -translate-x-1/2
-                                -translate-y-1/2
-                                w-[3%]
-                                h-[3%]
-                                min-w-[44px]
-                                min-h-[44px]
-                                bg-[<?php echo esc_attr( $atts['marker_background_color'] ); ?>]
-                                border-2
-                                border-green-300
-                                group-hover:border-indigo-500
-                                transition-colors
-                                duration-300
-                                hidden
-                                sm:block
-                                rounded-full"
-                            >
-                            </div>
-                            <?php
-                            break;
-                        case 'square':
-                            ?>
-                            <div class="
-                                absolute
-                                left-1/2
-                                transform
-                                -translate-x-1/2
-                                -translate-y-1/2
-                                w-[3%]
-                                h-[3%]
-                                min-w-[44px]
-                                min-h-[24px]
-                                bg-white
-                                border-2
-                                border-gray-200
-                                group-hover:border-indigo-500
-                                transition-colors
-                                duration-300
-                                rotate-75
-                                hidden
-                                sm:flex
-                                items-center
-                                justify-center"
-                            >
-                                <i class="fas fa-newspaper text-gray-400 group-hover:text-indigo-500 text-xs"></i>
-                            </div>
-                            <?php
-                            break;
-                        default:
-                            break;
-                    }
+            <?php
+            if ( 'no' !== $atts['marker_type'] ) {
                 ?>
+                <div class="
+                absolute
+                left-1/2
+                transform
+                -translate-x-1/2
+                -translate-y-1/2
+                w-[3%]
+                h-[3%]
+                min-w-[<?php echo esc_attr( $atts['marker_height'] ); ?>px]
+                min-h-[<?php echo esc_attr( $atts['marker_width'] ); ?>px]
+                bg-[<?php echo esc_attr( $atts['marker_background_color'] ); ?>]
+                group-hover:bg-[<?php echo esc_attr( $atts['marker_background_hover_color'] ); ?>]
+                border-<?php echo esc_attr( $atts['marker_border_type'] ); ?>
+                border-[<?php echo esc_attr( $atts['marker_border_color'] ); ?>]
+                border-<?php echo esc_attr( $atts['marker_border_style'] ); ?>
+                group-hover:border-[<?php echo esc_attr( $atts['marker_border_hover_color'] ); ?>]
+                rotate-<?php echo esc_attr( $atts['marker_rotation_degree'] ); ?>
+                hidden
+                sm:flex
+                items-center
+                justify-center
+                <?php
+                switch ($atts['marker_type']) {
+                    case 'circle':
+                        ?>
+                        rounded-full
+                        <?php
+                        break;
+                    default:
+                        break;
+                }
+                ?>
+                ">
+                    <?php
+                    if ( 'true' === $atts['add_icon'] ) {
+                        vc_icon_element_fonts_enqueue( $atts['i_type'] );
+                        $icon_class = $custom_icon_integration->get_element_icon_class( $atts );
+                        $icon_color = $custom_icon_integration->get_element_icon_color( $atts );
+                        ?>
+                        <i class="<?php echo esc_attr( $icon_class ); ?>" style="color: <?php echo esc_attr( $icon_color ); ?>"></i>
+                        <?php
+                    }
+                    ?>
 
+                </div>
+                <?php
+            }
+            ?>
             <div class="w-full sm:w-[48%] pl-0 sm:pl-[4%] md:pl-[6%]">
                 <img src="https://images.unsplash.com/photo-1516116216624-53e697fedbea" alt="Digital Media" class="w-full aspect-video object-cover rounded-lg shadow-lg">
             </div>
