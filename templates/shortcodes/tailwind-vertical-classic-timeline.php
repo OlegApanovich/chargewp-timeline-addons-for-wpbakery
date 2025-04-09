25053f1f7e05fca948fe5a6ae81821dc46d0c6e5
OlegApanovich <dollar4444@gmail.com>
2025-04-09 15:59:09 +0300

    Add default content to tailwind-vertical-timeline element

--- templates/shortcodes/tailwind-vertical-classic-timeline.php
+++ templates/shortcodes/tailwind-vertical-classic-timeline.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <div>
-	<div <?php $_this->output_shortcode_wrapper_attributes( [ 'class' => 'chargewp-tailwind-vertical-classic-timeline-container relative w-full p-6 md:p-8' ] ); ?>">
+	<div <?php $_this->output_shortcode_wrapper_attributes( [ 'class' => 'chargewp-tailwind-vertical-classic-timeline-container relative w-full p-6 md:p-8' ] ); ?>>
 		<div class="w-full max-w-[100%] mx-auto container">
 			<div class="relative w-full px-[2%] sm:px-[4%] md:px-[6%]">
 				<div class="absolute left-1/2 transform -translate-x-1/2 h-full w-<?php echo esc_attr( (int) $atts['baseline_width'] ); ?> bg-[<?php echo esc_attr( $atts['baseline_background'] ); ?>] hidden sm:block"></div>
