ef8a0d6d1cbfaa2a473fd7114daf4ed823af3553
OlegApanovich <dollar4444@gmail.com>
2025-12-21 11:14:58 +0200

    Fix phpstan constant issue #34

--- config/shortcodes/classic-horizontal-timeline.php
+++ config/shortcodes/classic-horizontal-timeline.php
@@ -291,7 +291,7 @@ return [
 	'params'                  => array_merge(
 		$params,
 		$items,
-		[ version_compare( WPB_VC_VERSION, '9.0', '<' ) ? vc_map_add_css_animation() : vc_config()->get_css_animation() ],
+		[ method_exists( vc_config(), 'get_css_animation' ) ? vc_config()->get_css_animation() : vc_map_add_css_animation() ],
 		chargewpwpbtimeline_config( 'element-params' )['common-bottom-params']
 	),
 ];
