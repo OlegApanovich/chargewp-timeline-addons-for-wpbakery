7d6939a15abacbc0149f6e2a2d04f56b65f15b38
OlegApanovich <dollar4444@gmail.com>
2026-07-31 19:17:52 +0300

    fix: #52 revert changes not related to task

--- src/Shortcodes/ChargeWpbShortcode.php
+++ src/Shortcodes/ChargeWpbShortcode.php
@@ -162,10 +162,8 @@ class ChargeWpbShortcode {
 	public function render_shortcode( array $atts, $content = null ): string {
 		$this->shortcode_atts = $atts;
 
-		$mapped_atts = vc_map_get_attributes( $this->wpb_shortcode->getShortcode(), $atts );
-
 		$payload = [
-			'atts'    => array_merge( $atts, $mapped_atts ),
+			'atts'    => vc_map_get_attributes( $this->wpb_shortcode->getShortcode(), $atts ),
 			'content' => $content,
 			'_this'   => $this,
 		];
